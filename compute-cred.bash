1bb5af12ee61e40bd740beaa7da1d302dda3f9cd
Chibuotu Amadi <chibuotu@gmail.com>
2020-11-05 04:44:19 +0100

    feat(bash): add script to generate instance

---
+++ compute-cred.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+: <<'END'
+Copyright (C) 2020 Gitcoin Core
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+END
+
+# .env loading in the shell
+dotenv () {
+  echo "- Load environmental variables"
+  set -a
+  [ -f .env ] && . .env
+  set +a
+}
+
+# Run dotenv on login
+dotenv
+
+# Run dotenv on every new directory
+cd () {
+  builtin cd $@
+  dotenv
+}
+
+echo "- Install packages"; yarn --frozen-lockfile
+
+echo "- Load Data into Graph"; yarn load; yarn graph || exit 1
+
+echo "- Compute Cred"; yarn score
